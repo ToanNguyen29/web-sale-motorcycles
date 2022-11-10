216fa4119740ef9ff287b2d9d7effa600e1754f6
ToanNguyen29 <102215511+ToanNguyen29@users.noreply.github.com>
2022-11-10 17:39:31 +0700

    Update create_table_web_saleMotorcycles.sql

--- query_sql/create_table_web_saleMotorcycles.sql
+++ query_sql/create_table_web_saleMotorcycles.sql
@@ -1,5 +1,5 @@
-Create database webSale_motorcycles_database;
-Use webSale_motorcycles_database;
+Create database web_Sale_motorcycles;
+Use web_Sale_motorcycles;
 
 CREATE TABLE MOTORCYCLES(
 	motors_id INT PRIMARY KEY,
@@ -7,21 +7,23 @@ CREATE TABLE MOTORCYCLES(
 	version varchar(50) not null,
 	motor_type varchar(50),
 	branch varchar(50) not null,
-    style varchar(50),
-    quantity INT not null default 0,
-    motor_desc text,
-    check_sale ENUM('sale','stop sale') not null default 'sale',
-    constraint motors_id_name_ver unique(motors_id, motors_name, version)
+    	style varchar(50),
+    	quantity INT not null default 0,
+    	motor_desc text,
+    	check_sale ENUM('sale','stop sale') not null default 'sale',
+    	constraint motors_id_name_ver unique(motors_id, motors_name, version)
 );
 
 CREATE TABLE A_MOTORCYCLE(
 	a_motor_id INT PRIMARY KEY,
 	motors_id INT not null,
 	color varchar(20),
-    prepaid_price FLOAT,
-    price FLOAT,
+    	prepaid_price FLOAT,
+    	price FLOAT,
+    	date_insert datetime default current_timestamp,
+    	date_update datetime,
 	check_sold ENUM('sold','ordered','not sold yet') default 'not sold yet',
-    foreign key(motors_id) References MOTORCYCLES(motors_id)
+    	foreign key(motors_id) References MOTORCYCLES(motors_id)
 );
 
 CREATE TABLE SPECIFICATION(
@@ -44,38 +46,39 @@ CREATE TABLE ROLE(
 CREATE TABLE USER(
 	user_id INT(5) zerofill PRIMARY KEY auto_increment ,
 	role_id INT,
-    email varchar(50),
-    user_pass varchar(50) not null,
-    user_name varchar(50) not null,
-    cccd Int,
-    num_phone INT,
-    address varchar(100),
-    check_delete ENUM('still','deleted') default 'still',
+    	email varchar(50),
+    	user_pass varchar(50) not null,
+    	user_name varchar(50) not null,
+    	cccd Int,
+    	num_phone INT,
+    	address varchar(100),
+    	check_delete ENUM('still','deleted') default 'still',
 	foreign key(role_id) References ROLE(role_id),
-    constraint email_check unique(email, check_delete)
+    	constraint email_check unique(email, check_delete)
 );
 
 CREATE TABLE BLOG(
 	blog_id INT Primary key auto_increment,
 	title text,
 	body text, 
-	date_write datetime not null default current_timestamp	
+	date_write datetime default current_timestamp,
+	date_update datetime
 );
 
 CREATE TABLE PAYMENT(
 	payment_id INT(6) zerofill PRIMARY KEY auto_increment,
 	user_id INT(5) zerofill,
-	a_motor_id INT not null,
-    name_cus varchar(50),
-    cccd Int,
-    date_buy datetime,
-    date_prepaid datetime,
-    money_prepaid float,
-    price FLOAT,
-    payment_menthod varchar(30),
-    status ENUM('not paid', 'paid', 'successful', 'cancel'),
+	a_motor_id INT,
+    	name_cus varchar(50),
+    	cccd Int,
+    	date_buy datetime,
+    	date_prepaid datetime,
+    	money_prepaid float,
+    	price FLOAT,
+    	payment_menthod varchar(30),
+    	status ENUM('not paid', 'paid', 'successful', 'cancel'),
 	foreign key(a_motor_id) References A_MOTORCYCLE(a_motor_id),
-    foreign key(user_id) References USER(user_id)
+    	foreign key(user_id) References USER(user_id)
 );
 
 CREATE TABLE COMMENT(
@@ -88,4 +91,6 @@ CREATE TABLE COMMENT(
 	 foreign key(blog_id) References BLOG(blog_id),
 	 foreign key(user_id) References USER(user_id)
 );
+
+
 
